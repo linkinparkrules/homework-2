0a67f5ecb68a1b229708351a243bec1ec36a22f9
linkinparkrules <nghiacao1997@gmail.com>
2022-06-18 10:42:48 +0700

    bo sung bai 1

--- lesson1/b1.js
+++ lesson1/b1.js
@@ -1,9 +1,13 @@
-let myString = `abcdef`;
+let myString = prompt("please enter a string", "abcdef");
 document.getElementById("initialString").innerHTML = myString;
-// chia string của thằng myString ra thành các mảng array
-const myArr = myString.split('');
-// đảo ngược các mảng array và nối chúng lại thành một string mới
-const newString = myArr.reverse().join("");
-document.getElementById("reverseString").innerHTML = newString;
+let newString;
+function reverse() {
+    // chia string của thằng myString ra thành các mảng array
+    const myArr = myString.split('');
+    // đảo ngược các mảng array và nối chúng lại thành một string mới
+    newString = myArr.reverse().join("");
+    document.getElementById("reverseString").innerHTML = newString;
+}
+reverse();
 console.log(myString);
 console.log(newString);
